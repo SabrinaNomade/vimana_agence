3a505a8dc021924df50bb2c920eb6c22bed8b868
sabrina <berrandousabrina@gmail.com>
2025-02-09 18:33:12 +0100

    Ajout du dossier admin avec deux fichiers

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Controller\Admin;
+
+use App\Entity\User;  
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin_dashboard')]
+    public function index(): Response
+    {    
+        // Récupérer le service depuis le conteneur
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+
+        // Générer l'URL vers la gestion des utilisateurs
+        $url = $adminUrlGenerator
+            ->setController(UserCrudController::class)
+            ->generateUrl();
+
+        return $this->redirect($url);
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Vimana Agence');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-users', User::class);
+    }
+}
+
+
